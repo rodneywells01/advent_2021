b24de13a2bc4dff770100cf7a779aa2d8a5ec99c
Rodney Wells <rodneywells01@gmail.com>
2021-12-06 01:09:18 -0600

    Formatting

--- 5.py
+++ 5.py
@@ -1,33 +1,38 @@
-from collections import defaultdict 
+from collections import defaultdict
 
 vents = defaultdict(int)
+
+
 def record_vent(start, end):
-	num_vents = max(abs(end[1] - start[1]), abs(end[0] - start[0])) + 1
-	direction_x = int((end[0] - start[0])/(num_vents - 1))
-	direction_y = int((end[1] - start[1])/(num_vents - 1))
-	
-	for idx in range(num_vents):
-		# Create a key of the format "y,x" for a vent. 
-		key = f"{start[1] + idx * direction_y},{start[0] + idx * direction_x}"
+    """
+    Given a start and end index,
+    """
+    num_vents = max(abs(end[1] - start[1]), abs(end[0] - start[0])) + 1
+    direction_x = int((end[0] - start[0]) / (num_vents - 1))
+    direction_y = int((end[1] - start[1]) / (num_vents - 1))
 
-		if vents.get(key):
-			vents[key] += 1
-		
-		
-def count_multiple_vents(): 
-	return sum([num_vents > 1 for num_vents in vents.values()])
-	
+    for idx in range(num_vents):
+        # Create a key of the format "y,x" for a vent.
+        key = f"{start[1] + idx * direction_y},{start[0] + idx * direction_x}"
 
-with open('input/5.txt') as f:
-	lines = f.readlines()
+        if vents.get(key):
+            vents[key] += 1
 
-	for line in lines:
-		vent_coordinates = line.rstrip().split()
-		start = [int(coordinate) for coordinate in vent_coordinates[0].split(",")]
-		end = [int(coordinate) for coordinate in vent_coordinates[2].split(",")]
 
-		record_vent(start, end)
+def count_multiple_vents():
+    return sum([num_vents > 1 for num_vents in vents.values()])
 
-	total = count_multiple_vents()
-	print(f"Total: {total}")
-	# assert total == 18144
+
+with open("input/5.txt") as f:
+    lines = f.readlines()
+
+    for line in lines:
+        vent_coordinates = line.rstrip().split()
+        start = [int(coordinate) for coordinate in vent_coordinates[0].split(",")]
+        end = [int(coordinate) for coordinate in vent_coordinates[2].split(",")]
+
+        record_vent(start, end)
+
+    total = count_multiple_vents()
+    print(f"Total: {total}")
+    # assert total == 18144
