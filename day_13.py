8a7012a77d81021bfbe9cc89c035aa819165704d
Rodney Wells <rodneywells01@gmail.com>
2021-12-13 14:46:18 -0600

    Signficant cleanup of day 13

--- day_13.py
+++ day_13.py
@@ -2,10 +2,9 @@ def parse_input():
     # Parse file input
     dot_positions = list()
     fold_instructions = list()
-
     reading_fold_instructions = False 
+
     with open("input/13.txt") as f:
-        # Each line contains 2 caves 
         for line in f.readlines():
             line = line.rstrip()
 
@@ -20,16 +19,11 @@ def parse_input():
     return dot_positions, fold_instructions
 
 
-dot_positions, fold_instructions = parse_input()
-print(dot_positions)
-print(fold_instructions)
-
-
 # Build the paper 
 def fold(paper, fold_instruction): 
-    pieces = fold_instruction.split("=")
-    fold_plane = pieces[0]
-    fold_line = int(pieces[1])
+    instruction_pieces = fold_instruction.split("=")
+    fold_plane = instruction_pieces[0]
+    fold_line = int(instruction_pieces[1])
 
     # Find the smaller, apply the smaller to the larger. 
     folded_pieces = []
@@ -37,42 +31,29 @@ def fold(paper, fold_instruction):
         folded_pieces.append(paper[:fold_line])
         folded_pieces.append(paper[fold_line + 1:])
     elif fold_plane == "x":
-        folded_left_piece = [] 
-        folded_right_piece = [] 
-        for row in paper:
-            folded_left_piece.append(row[:fold_line])
-            folded_right_piece.append(row[fold_line + 1:])
-        folded_pieces = [folded_left_piece, folded_right_piece]
-
+        folded_pieces.append([row[:fold_line] for row in paper])
+        folded_pieces.append([row[fold_line + 1:] for row in paper])
     else: 
         raise Exception(f"We can't handle fold_plane {fold_plane}")
-
-
-    # print(f"Here are pieces for fold {fold_plane}={str(fold_line)}")
-    # for piece in folded_pieces:
-    #     pretty_print(piece)
-    #     print()
 
 
     # Now apply the fold 
     # TODO: Handle folds of unequal size 
     folded_paper = folded_pieces[0]
     for row_idx in range(len(folded_pieces[1])):
-        for col_idx in range(len(folded_pieces[1][row_idx])):
-            value = folded_pieces[1][row_idx][col_idx]
-            
+        for col_idx, value in enumerate(folded_pieces[1][row_idx]):
             # Set the value on the other half 
             if value is "#":
+                # Determine the correct row and col idxs on the folded page
                 folded_row_idx = row_idx
                 folded_col_idx = col_idx
-                
                 if fold_plane == "y": 
                     folded_row_idx = len(folded_pieces[1]) - 1 - row_idx
                 else: 
                     folded_col_idx = len(folded_pieces[1][row_idx]) - 1 - col_idx
 
                 # Update the value on the folded sheet. 
-                folded_pieces[0][folded_row_idx][folded_col_idx] = value
+                folded_paper[folded_row_idx][folded_col_idx] = value
 
     print(f"Here is the fold on {fold_plane}={str(fold_line)}")
     pretty_print(folded_paper)
@@ -81,11 +62,9 @@ def fold(paper, fold_instruction):
     return folded_paper
 
 def generate_paper(dot_positions): 
-    paper = [] 
     # Determine dimensions of the paper. 
     max_x = 0
     max_y = 0 
-
     for dot in dot_positions: 
         if dot[1] + 1 > max_y:
             max_y = dot[1] + 1 
@@ -94,31 +73,19 @@ def generate_paper(dot_positions):
 
     # Build the paper 
     paper = [["." for _ in range(max_x)] for _ in range(max_y)]
-
-    pretty_print(paper)
-
     for dot in dot_positions: 
-        print(dot)
         paper[dot[1]][dot[0]] = "#"
 
     return paper 
 
 def pretty_print(paper): 
     for row in paper: 
-        pretty_row = ""
-        for val in row: 
-            pretty_row += f"{val} "
+        pretty_row = " ".join([val for val in row])
         print(pretty_row)
 
 def solve_problem(dot_positions, fold_instructions): 
     paper = generate_paper(dot_positions)
-
-    print("Original")
-    pretty_print(paper)
-    # return paper 
-
     for fold_instruction in fold_instructions: 
-        # paper = fold(fold_instruction, paper)
         paper = fold(paper, fold_instruction)
 
     dot_count = count_dots(paper)
@@ -135,6 +102,11 @@ def count_dots(paper):
 
     return dot_count
 
-final_result = solve_problem(dot_positions, fold_instructions)
-# pretty_print(final_result)
-print(final_result)
+def main():
+    dot_positions, fold_instructions = parse_input()
+    final_result = solve_problem(dot_positions, fold_instructions)
+    print(final_result)
+
+
+if __name__ == "__main__":
+    main()
